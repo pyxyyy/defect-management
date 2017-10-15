4274762778cb2e38217978162f080ff75c567a13
Hamsyari <hamsyari22@gmail.com>
2017-10-15 06:49:41 -0700

    Add slight animation to 'Archives' page

---
+++ consumer/src/components/NavBar/NavBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import logo from '../../img/logo.svg';
+import './NavBar.css';
+import { connect } from 'react-redux';
+
+class NavBar extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div className="NavBar">
+                <div className="NavBar-flight" onClick={() => {this.props.nav("Landing")}}>SQ666</div>
+                <div className="NavBar-menu">
+                    <div className="NavBar-defects" onClick={() => this.props.nav("Defects")}>Defects</div>
+                    <div className="NavBar-analytics" onClick={() => {this.props.nav("Analytics")}}>Analytics</div>
+                    <div className="NavBar-archive" onClick={() => {this.props.nav("Archive")}}>Archive</div>
+                    <img src={logo} className="NavBar-logo" alt="logo" />
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        page: state.page
+    }
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        nav: (page) => {
+            dispatch({
+                type: 'nav',
+                page: page
+            });
+        },
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
--- engineer/src/pages/ArchivePage/ArchivePage.js
+++ engineer/src/pages/ArchivePage/ArchivePage.js
@@ -3,6 +3,7 @@ import './ArchivePage.css';
 import {FontAwesome} from 'react-fontawesome';
 import {Row, Col} from 'react-bootstrap';
 import TimelineEvent from "./TimelineEvent/TimelineEvent";
+import ScrollAnimation from 'react-animate-on-scroll';
 
 class ArchivePage extends Component {
 
@@ -14,24 +15,72 @@ class ArchivePage extends Component {
                 <div className="div-row">
                     <div className="div-col-10 timeline-list">
                         <ul>
-                            <li id="2017"><TimelineEvent timelineDate="1 September 2017, Friday" defects="3"/></li>
-                            <li><TimelineEvent timelineDate="26 August 2017, Saturday" defects="35"/></li>
-                            <li><TimelineEvent timelineDate="3 June 2017, Saturday" defects="27"/></li>
-                            <li><TimelineEvent timelineDate="26 May 2017, Friday" defects="58"/></li>
-                            <li><TimelineEvent timelineDate="22 April 2017, Saturday" defects="17"/></li>
-                            <li><TimelineEvent timelineDate="16 April 2017, Sunday" defects="88"/></li>
-                            <li><TimelineEvent timelineDate="1 April 2017, Saturday" defects="16"/></li>
-                            <li><TimelineEvent timelineDate="24 February 2017, Friday" defects="85"/></li>
-                            <li id="2016"><TimelineEvent timelineDate="15 Oct 2017, Sunday" defects="66"/></li>
-                            <li><TimelineEvent timelineDate="28 December 2016, Wednesday" defects="77"/></li>
-                            <li><TimelineEvent timelineDate="27 December 2016, Tuesday" defects="18"/></li>
-                            <li><TimelineEvent timelineDate="26 December 2016, Monday" defects="22"/></li>
+                            <li id="2017">
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="1 September 2017, Friday" defects="3"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="26 August 2017, Saturday" defects="35"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="3 June 2017, Saturday" defects="27"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="26 May 2017, Friday" defects="58"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="22 April 2017, Saturday" defects="17"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="16 April 2017, Sunday" defects="88"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="1 April 2017, Saturday" defects="16"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="24 February 2017, Friday" defects="85"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li id="2016">
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="15 Oct 2017, Sunday" defects="66"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="28 December 2016, Wednesday" defects="77"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="27 December 2016, Tuesday" defects="18"/>
+                                </ScrollAnimation>
+                            </li>
+                            <li>
+                                <ScrollAnimation animateIn='fadeIn' animateOnce="true">
+                                    <TimelineEvent timelineDate="26 December 2016, Monday" defects="22"/>
+                                </ScrollAnimation>
+                            </li>
                         </ul>
                     </div>
                     <div className="div-col-2 timeline-nav">
-                        <ul class="timeline-year">
+                        <ul className="timeline-year">
                             <li>
-                                <ul class="inner">
+                                <ul className="inner">
                                     <li><a href="#2017">2017</a></li>
                                     <li><a href="#2016">2016</a></li>
                                 </ul>
