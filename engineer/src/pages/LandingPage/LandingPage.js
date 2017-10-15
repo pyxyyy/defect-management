b61e6978f21beb0bcb58fb224047c3d01ccb7889
Lee Pei Xuan <lee.pei.xuan.2011@vjc.sg>
2017-10-15 08:03:41 -0700

    implement landing page

--- engineer/src/pages/LandingPage/LandingPage.js
+++ engineer/src/pages/LandingPage/LandingPage.js
@@ -9,7 +9,35 @@ class LandingPage extends Component {
     render() {
         return (
             <div className="LandingPage">
-                <button onClick={() => {this.props.nav("Defects")}}>click</button>
+                <span className="LandingPage-header">These are your flights for today: </span>
+                <span className="LandingPage-subheader">Select a flight to view and manage its defects.</span>
+                <div className="LandingPage-row">
+                    <div className="LandingPage-col">
+                        <div>03:40</div>
+                        <div className="LandingPage-row-flight1"></div>
+                        <span>SQ 634</span>
+                    </div>
+                    <div className="LandingPage-col">
+                        <div>03:52</div>
+                        <div className="LandingPage-row-flight2" onClick={() => {this.props.nav("Defects")}}></div>
+                        <span>SQ 31</span>
+                    </div>
+                    <div className="LandingPage-col">
+                        <div>07:22</div>
+                        <div className="LandingPage-row-flight3"></div>
+                        <span>SQ 5906</span>
+                    </div>
+                    <div className="LandingPage-col">
+                        <div>08:55</div>
+                        <div className="LandingPage-row-flight4"></div>
+                        <span>SQ 636</span>
+                    </div>
+                    <div className="LandingPage-col">
+                        <div>08:55</div>
+                        <div className="LandingPage-row-flight5"></div>
+                        <span>SQ 12</span>
+                    </div>
+                </div>
             </div>
         );
     }
