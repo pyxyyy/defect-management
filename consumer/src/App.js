9a793091300e52476ac54da68e49e65cf370d211
drashtizglue <31018185+drashtizglue@users.noreply.github.com>
2017-10-15 09:09:31 -0700

    Made the consumer interface.

--- consumer/src/App.js
+++ consumer/src/App.js
@@ -1,19 +1,84 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import seat from './img/seat.svg';
+import popcorn from './img/popcorn.svg';
+import headphones from './img/headphones.svg';
+import t from './img/t.svg';
+import light from './img/light.svg';
+import o from './img/o.svg';
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttonA : false,
+            buttonB : false,
+            buttonC : false,
+        }
+    }
+
+    markClickedA(){
+        this.setState({
+            buttonA : !this.state.buttonA
+        })
+    }
+    markClickedB(){
+        this.setState({
+            buttonB : !this.state.buttonB
+        })
+    }
+    markClickedC(){
+        this.setState({
+            buttonC : !this.state.buttonC
+        })
+    }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      let styleButtonA = this.state.buttonA ? "SelectDefect-button-clicked" : "SelectDefect-button-unclicked";
+      let styleButtonB = this.state.buttonB ? "SelectDefect-button-clicked" : "SelectDefect-button-unclicked";
+      let styleButtonC = this.state.buttonC ? "SelectDefect-button-clicked" : "SelectDefect-button-unclicked";
+
+      return (
+        <div className="Defects">
+                <span className="Instructions">
+                If you noticed any defects, help us by logging it below. <br/>
+                Logging this defect will ensure that your experience with us is top-notch!
+                </span>
+            <div className="SelectDefect">
+                <div className="SelectDefect-1">
+
+                    <div className={styleButtonA} onClick={() => { this.markClickedA() }}>
+                        <img src={seat} alt="Chair" />
+                        <span>Chair</span>
+                    </div>
+
+                    <div className="SelectDefect-button">
+                        <img src={popcorn} alt="In-Flight Entertainment" />
+                        <span>In-Flight Entertainment</span>
+                    </div>
+
+                    <div className={styleButtonB} onClick={() => { this.markClickedB() }}>
+                        <img src={headphones} alt="Audio Jack" />
+                        <span>Audio Jack</span>
+                    </div>
+                </div>
+                <div className="SelectDefect-2">
+                    <div className={styleButtonC} onClick={() => { this.markClickedC() }}>
+                        <img src={t} alt="Meal tray" />
+                        <span>Meal Tray</span>
+                    </div>
+                    <div className="SelectDefect-button">
+                        <img src={light} alt="Reading Light" />
+                        <span>Reading Light</span>
+                    </div>
+                    <div className="SelectDefect-button">
+                        <img src={o} alt="Others" />
+                        <span>Others</span>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
   }
 }
